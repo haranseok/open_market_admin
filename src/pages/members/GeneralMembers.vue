d3b606078b60a7a258f999872eaef1c6da8597a5
haran seok <gkfks222@gmail.com>
2023-07-21 14:40:11 +0900

    회원관리 페이지 UI 구현 완료

--- src/pages/members/GeneralMembers.vue
+++ src/pages/members/GeneralMembers.vue
@@ -10,13 +10,17 @@
         <td>{{ row.id }}</td>
         <td>{{ row.name }}</td>
         <td>{{ row.phone }}</td>
-        <td>
-          <select v-model="row.status">
-            <option :value="1">회원</option>
-            <option :value="2">휴면계정</option>
-            <option :value="3">탈퇴</option>
-          </select>
+        <td class="select-wrap">
+          <div class="select-box flex cp">
+            <select v-model="row.status" class="cp">
+              <option :value="1">회원</option>
+              <option :value="2">휴면계정</option>
+              <option :value="3">탈퇴</option>
+            </select>
+            <v-icon>mdi-menu-down</v-icon>
+          </div>
         </td>
+        <td>{{ row.login }}</td>
         <td>{{ row.createDate }}</td>
       </template>
     </BaseTable>
@@ -26,13 +30,13 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 import BaseTable from "@/components/tables/BaseTable.vue";
-const seleted = ref("");
 const th = ref([
   "전체선택",
   "id",
   "회원 명",
   "연락처",
   "상태",
+  "로그인 방법",
   "등록일자",
   "수정",
   "삭제",
@@ -44,6 +48,7 @@ const items = ref([
     name: "홍길동",
     phone: "010-1111-1111",
     status: 1,
+    login: "kakao",
     createDate: "2023-07-17",
   },
   {
@@ -51,6 +56,7 @@ const items = ref([
     name: "홍길동2",
     phone: "010-1111-1111",
     status: 1,
+    login: "자사회원가입",
     createDate: "2023-07-17",
   },
   {
@@ -58,6 +64,7 @@ const items = ref([
     name: "홍길동3",
     phone: "010-1111-1111",
     status: 2,
+    login: "never",
     createDate: "2023-07-17",
   },
 ]);
@@ -66,4 +73,28 @@ const doDelete = (e: any) => {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.select-wrap {
+  width: 100px;
+  text-align: center;
+}
+.select-box {
+  position: relative;
+  width: 80px;
+  margin-left: 5px;
+  align-items: center;
+  padding: 0;
+  background: rgb(236, 236, 236);
+  border-radius: 3px;
+  select {
+    width: 100%;
+    padding: 5px;
+    z-index: 2;
+  }
+  .v-icon {
+    position: absolute;
+    right: 0;
+    z-index: 1;
+  }
+}
+</style>
--- src/pages/members/SalesMembers.vue
+++ src/pages/members/SalesMembers.vue
@@ -1,7 +1,58 @@
 <template>
-  <div>판매회원</div>
+  <div class="base-table">
+    <BaseTable
+      :headers="th"
+      :list="items"
+      :btn-text="'일괄 삭제'"
+      @btn-event="doDelete"
+    >
+      <template #list="{ row }">
+        <td>{{ row.id }}</td>
+        <td>{{ row.store }}</td>
+        <td>{{ row.number }}</td>
+        <td>{{ row.address }}</td>
+        <td>{{ row.category }}</td>
+        <td>{{ row.name }}</td>
+        <td>{{ row.phone }}</td>
+        <td>{{ row.createDate }}</td>
+      </template>
+    </BaseTable>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from "vue";
+import BaseTable from "@/components/tables/BaseTable.vue";
+
+const th = ref([
+  "전체선택",
+  "id",
+  "쇼핑몰",
+  "사업자번호",
+  "사업자 주소",
+  "업종",
+  "판매자명",
+  "연락처",
+  "등록일자",
+  "수정",
+  "삭제",
+]);
+
+const items = ref([
+  {
+    id: "admin",
+    store: "a Store",
+    number: "000-00-00000",
+    address: "서울시 강남구 00동 ",
+    category: "도매 및 소매업/ㅇㅇ",
+    name: "홍길동",
+    phone: "010-0000-0000",
+    createDate: "2023-07-21",
+  },
+]);
+const doDelete = (e: any) => {
+  console.log(e);
+};
+</script>
 
 <style lang="scss" scoped></style>
